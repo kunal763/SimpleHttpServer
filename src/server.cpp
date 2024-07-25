6b2b79cfd1d2735dc001466cf9eedd99bb4ff34c
Kunal Singh <ksingh112113114@gmail.com>
2024-07-25 13:42:27 +0530

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -38,53 +38,66 @@ void handle_client(int client_socket, const std::string &client_ip, const std::s
         std::string path = request_line.substr(method_end + 1, path_end - method_end - 1);
         std::string not_found_msg = "HTTP/1.1 404 Not Found\r\n\r\n";
         std::string pass_message = "HTTP/1.1 200 OK\r\n\r\n";
-        if (path.substr(0, 6) == "/echo/")
+        if (method == "GET")
         {
-          std::string client_str = path.substr(6);
+          if (path.substr(0, 6) == "/echo/")
+          {
+            std::string client_str = path.substr(6);
 
-          std::string server_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(client_str.length());
-          server_response += "\r\n\r\n" + client_str;
-          send(client_socket, server_response.c_str(), server_response.length(), 0);
-        }
-        else if (path.find("/user-agent") != std::string::npos)
-        {
-          size_t ua_pos = request.find("User-Agent: ");
-          if (ua_pos != std::string::npos)
-          {
-            size_t ua_end = request.find("\r\n", ua_pos);
-            std::string user_agent = request.substr(ua_pos + 12, ua_end - ua_pos - 12);
-            std::string server_resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent.length());
-            server_resp += "\r\n\r\n" + user_agent;
-            send(client_socket, server_resp.c_str(), server_resp.length(), 0);
+            std::string server_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(client_str.length());
+            server_response += "\r\n\r\n" + client_str;
+            send(client_socket, server_response.c_str(), server_response.length(), 0);
           }
-        }
-        else if (path.find("/files/") != std::string::npos)
-        {
-          std::string file_name = path.substr(7);
-          std::string file_path = directory + file_name;
-          if (fs::exists(file_path))
+          else if (path.find("/user-agent") != std::string::npos)
           {
-            auto file_size = fs::file_size(file_path);
-            // I am now reading the file
-            std::ifstream file(file_path, std::ios::binary);
-            if (file)
+            size_t ua_pos = request.find("User-Agent: ");
+            if (ua_pos != std::string::npos)
             {
-              std::string file_contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-              std::string resp = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(file_size) + "\r\n\r\n";
-              resp += file_contents;
-              std::cout << "File exists: " << file_path << std::endl;
-              send(client_socket, resp.c_str(), resp.length(), 0);
+              size_t ua_end = request.find("\r\n", ua_pos);
+              std::string user_agent = request.substr(ua_pos + 12, ua_end - ua_pos - 12);
+              std::string server_resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent.length());
+              server_resp += "\r\n\r\n" + user_agent;
+              send(client_socket, server_resp.c_str(), server_resp.length(), 0);
             }
           }
-          else
+          else if (path.find("/files/") != std::string::npos)
           {
+            std::string file_name = path.substr(7);
+            std::string file_path = directory + file_name;
+            if (fs::exists(file_path))
+            {
+              auto file_size = fs::file_size(file_path);
+              // I am now reading the file
+              std::ifstream file(file_path, std::ios::binary);
+              if (file)
+              {
+                std::string file_contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+                std::string resp = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(file_size) + "\r\n\r\n";
+                resp += file_contents;
+                std::cout << "File exists: " << file_path << std::endl;
+                send(client_socket, resp.c_str(), resp.length(), 0);
+              }
+            }
+            else
+            {
+              send(client_socket, not_found_msg.c_str(), not_found_msg.length(), 0);
+            }
+          }
+          else if (path != "/")
             send(client_socket, not_found_msg.c_str(), not_found_msg.length(), 0);
+          else
+            send(client_socket, pass_message.c_str(), pass_message.length(), 0);
+        }
+        if (method=="POST")
+        {
+            if (path.find("/files/") != std::string::npos)
+          {
+            std::string file_name = path.substr(7);
+            std::string file_path = directory + file_name;
+            std::cout<<"this is post method\n";
           }
         }
-        else if (path != "/")
-          send(client_socket, not_found_msg.c_str(), not_found_msg.length(), 0);
-        else
-          send(client_socket, pass_message.c_str(), pass_message.length(), 0);
+        
         // Send a basic HTTP response
       }
     }
@@ -107,7 +120,7 @@ int main(int argc, char **argv)
       directory = argv[i + 1];
     }
   }
- 
+
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
   {
